e86c855d159e6d8e803135ce66b20f1077731bda
Enconu <95133846+Enconu@users.noreply.github.com>
2021-12-15 13:02:28 +0300

    Add files via upload

---
+++ Tomazuko/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+	<title>Tomazuko</title>
+</head>
+<body>
+	
+	<header class="site-header sticky-top py-1">
+	  <nav class="container d-flex flex-column flex-md-row justify-content-between">
+	  
+	 
+	  
+	    <a class="py-2" href="#" aria-label="Product">
+		
+	      
+		<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+		<html>
+ 
+			
+	    </a>
+	    <a class="btn btn-outline-success" href="#">GDPS</a>
+	    <a class="btn btn-outline-primary" href="#">Discord</a>
+	    <a class="btn btn-outline-secondary" href="#">Support</a>
+		<button class="btn btn-danger">Demonlist</button>
+
+	  </nav>
+	</header>
+	
+
+			<main>
+			  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+			    <div class="col-md-5 p-lg-5 mx-auto my-5">
+			      <h1 class="display-4 fw-normal">Hopi Fat GDPS</h1>
+			      <p class="lead fw-normal"></p>
+			      
+				  <a class="btn btn-outline-secondary" href="#">Windows</a>
+				  <a class="btn btn-outline-secondary" href="#">Android</a>
+			    </div>
+			    <div class="product-device shadow-sm d-none d-md-block"></div>
+			    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+			  </div>
+			  <script>
+    window.list_length = 75;
+    window.extended_list_length = 150
+	</script><link ref="canonical" href="https://pointercrate.com/demonlist/"></head><body style="z-index:-10"><div style="width: 100%;height: 100%;position: fixed;top: 0;left: 0;background-size: cover;background-repeat: repeat-y;pointer-events: none; z-index:-1"></div><header><nav class="center collapse underlined see-through"><div class="nav-icon" style="margin-right: auto"><a href="/"><img src="/static/images/logo.png" style="height:50px"></a></div><div class="nav-group"><a class="nav-item hover white" href="/documentation/"><span style="display:flex; flex-direction:column;">
+    <!-- Demonlist Sidebar Ad #2 -->
+<ins class="adsbygoogle"
+style="display:block"
+data-ad-client="ca-pub-3064790497687357"
+data-ad-slot="3380750697"
+data-ad-format="auto"
+data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</body>
+</html>
+<html>
+ <head>
+  <meta charset="utf-8">
+
+
+  </style>
+ </head>
+ <body>
+  <p></p>
+ </body>
+</html>
+
+
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1251">
+<title> </title>
+</head>
+<body>
+ 
+</p>
+</body>
+</html>
+
+
+
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Цвет фона</title>
+  <style>
+   body {
+    background: #D3D3D3; /* Цвет фона */
+   }
+  </style>
+ </head>
+ <body>
+  <p></p>
+ </body>
+</html>
+</html>
+
+
+
+<p style="text-decoration:underline">
+
+
+
+<ul class="logos-list">
+  <li class="logos-list__item"><img class="logos-list__image" src="https://vignette.wikia.nocookie.net/logopedia/images/b/b8/Levi%27s_logo.png/revision/latest?cb=20130505012656" alt="Levi's" width="100">   </li>
+  <li class="logos-list__item">
+    <img class="logos-list__image" src="https://www.canva.com/learn/wp-content/uploads/2015/09/3-NBC-tb-1456x0.png" alt="NBC" width="100">
+  </li>
+  <li class="logos-list__item">
+    <img class="logos-list__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Burger_King_Logo.svg/2000px-Burger_King_Logo.svg.png" alt="Burger King" width="100">
+  </li>
+</ul>
+
+
